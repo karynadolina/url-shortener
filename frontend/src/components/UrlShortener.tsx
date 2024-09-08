531ba6ce72286f13f50ca852b44675b609833c62
karinadolina <karyna.apiekisheva@nure.ua>
2024-09-08 18:20:46 +0300

    fix build

--- frontend/src/components/UrlShortener.tsx
+++ frontend/src/components/UrlShortener.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { useLocation, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Container = styled.div`
